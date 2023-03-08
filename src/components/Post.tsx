7c81d0d60c935eda3f0620fe45bf5a8c368187f7
mohammad-a-hossain <adam.eve007@gmail.com>
2023-03-08 17:10:03 +0600

    ui-sidebar navigation design complelete

---
+++ src/components/Post.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { useState } from 'react';
+
+ const Post:React.FC = () => {
+  const [show,setShow] = useState(false)
+
+  const showForm = ()=>{
+     setShow(true)
+  }
+  const closemode=()=>{
+    setShow(false)
+  }
+  return (
+    <>
+    <div  className='relative flex justify-between w-full h-10 gap-1 p-2 mb-3 bg-slate-300'> 
+       <img src="https://htmldemo.net/adda/adda/assets/images/profile/profile-small-37.jpg" alt="imag" className='w-6 h-6 rounded-lg' />
+        <input type="search" id="search" className="block p-3 pl-2 text-gray-900 border border-gray-300 rounded-lg w-60 text-sx bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+       
+       <button onClick={showForm} type="submit" className="px-2 text-xs font-medium text-white bg-indigo-500 rounded-lg hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          post
+          </button> 
+          
+
+
+    </div>
+
+
+
+{ show ?
+   ( <div  className="absolute left-0 right-0 z-50 flex flex-col items-center justify-center mx-auto my-auto space-y-4 duration-1000 delay-700 bg-transparent top-12 h-22 ">
+ 
+   <div className="w-4/12 p-2 bg-white rounded shadow-xl">
+   <span  onClick={()=>closemode()} className='cursor-pointer'>x</span>
+     <form className="mt-3 space-y-2">
+       <input type="textarea" className="w-full h-12 px-3 border text-clip overflow-hidden ... border-gray-800 rounded" placeholder="whtat you say" />
+      
+ 
+       <div className="flex justify-start text-red-500">
+         
+         <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4 cursor-pointer">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+       <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+      </svg>
+
+
+       </div>
+ 
+       <button className="w-full px-2 py-2 text-xs font-medium text-center text-white bg-blue-900 rounded-md">post</button>
+     </form>
+ 
+   </div>
+ </div> ):null }
+
+ 
+
+  
+</>
+  )
+}
+export default Post
+
+
+/* 
+
+
+<button data-modal-target="popup-modal"
+ data-modal-toggle="popup-modal"
+  class="block text-white bg-blue-700 hover:bg-blue-800 
+  focus:ring-4 focus:outline-none focus:ring-blue-300
+   font-medium rounded-lg text-sm px-5 py-2.5 text-center 
+   dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    type="button">
+  Toggle modal
+</button>
+
+<div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+    <div class="relative w-full h-full max-w-md md:h-auto">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="p-6 text-center">
+                <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
+                <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                    Yes, I'm sure
+                </button>
+                <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+*/
