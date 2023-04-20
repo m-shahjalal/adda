b1697578714f04fd2cda6382efb1f0cff1607f19
Naiem Islam <naiem4288@gmail.com>
2023-04-20 14:51:20 +0600

    implemented login mutation

--- src/lib/mutation.ts
+++ src/lib/mutation.ts
@@ -12,3 +12,16 @@ export const REGISTER_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($input: UsersPermissionsLoginInput!) {
+    login(input: $input) {
+      jwt
+      user {
+        id
+        email
+        username
+      }
+    }
+  }
+`;
